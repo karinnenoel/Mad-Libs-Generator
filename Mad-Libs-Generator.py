cf66d055f1646d3da87b08e19cb0f40486b1f723
Karinne App <k.app@students.chapelfield.org>
2019-06-03 10:03:57 -0400

    Add another mad lib

--- Mad-Libs-Generator.py
+++ Mad-Libs-Generator.py
@@ -1,11 +1,21 @@
 
 #Mad Libs Generator
 
-print ("hello user welcome to the Mab Lib Generator")
+print ("Hello user welcome to the Mab Lib Generator!")
 
-name = input("type your name")
-place = input("type the name of a place (example- classroom)")
-verb = input("type a past tense verb (example- caught)")
-animal = input("type the name of your favorite animal")
+name = input("Type your name.")
+place = input("Type the name of a place. (example- classroom)")
+verb = input("Type a past tense verb. (example- caught)")
+animal = input("Type the name of your favorite animal.")
 
 print ('Today '  + name +  ' went to the ' + place + ' and then they ' + verb + ' a ' + animal + '.')
+
+print ('Now lets try another one!')
+
+holiday = input("Type the name of a holiday.")
+relative = input("Type a kind of relative. (example- brother)")
+adj = input("Type an an adjective or a describing word.")
+gift = input("Type the name of a gift/ a gift you would get for a family member.")
+
+
+print ('On ' + holiday + ' I got my ' + relative + ' a ' + adj + ' ' + gift + '.')
